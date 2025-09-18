f18f079fe82af673dab731b1455e8966ea21f476
hskim19-yeet <hskim19@asu.edu>
2025-09-18 14:53:52 -0700

    6 modified to main

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def admin():
     return render_template("admin.html")
 
+@app.route("/wallet")
+def wallet():
+    return render_template("wallet.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
